b9cc4147c64f4b1969dd2c38402c1bdb287118ec
Survivor75 <ashutoshnarang80@gmail.com>
2017-03-20 16:26:55 +0530

    remove.js

---
+++ Interactive_JavaScript/remove.js
@@ -0,0 +1,6 @@
+var button = document . querySelector (" button ") ;
+function once () {
+console . log (" Done .") ;
+button.removeEventListener ("click", once );
+}
+button.addEventListener("click", once );
