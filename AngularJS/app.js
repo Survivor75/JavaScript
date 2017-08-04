60e891973c424ce5d1e8f8c52017a7a9e6952284
Survivor75 <ashutoshnarang80@gmail.com>
2017-08-04 22:19:51 +0530

    init

---
+++ AngularJS/app.js
@@ -0,0 +1,7 @@
+//app.js
+var myModule = angular.module('Angello', []);
+
+myModule.factory(’AngelloHelper’, function() { });
+myModule.service(’AngelloModel’, function() { });
+myModule.controller(’MainCtrl’, function() { });
+myModule.directive(’story’, function() { });
