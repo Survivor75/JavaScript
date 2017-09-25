d5b826a51b3a1eb09fefda8f124112586c725b42
Survivor75 <ashutoshnarang80@gmail.com>
2017-09-26 00:51:35 +0530

    Modules

--- AJAX.js
+++ AJAX.js
@@ -5,8 +5,10 @@ function loadDoc() {
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
      document.getElementById("demo").innerHTML = this.responseText;
+     console.log("Hello")
     }
   };
   xhttp.open("GET", "https://cors-anywhere.herokuapp.com/http://carnes.cc/code/ajax_example.txt", true);
   xhttp.send();
+  console.log("World");
 }
