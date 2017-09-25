5bdcbeaf0e34a9b33f6598e390828a14e2ea9db3
Survivor75 <ashutoshnarang80@gmail.com>
2017-09-26 00:41:34 +0530

    Nuggets

---
+++ AJAX.js
@@ -0,0 +1,12 @@
+// AJAX = Asynchronous JavaScript And XML
+
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "https://cors-anywhere.herokuapp.com/http://carnes.cc/code/ajax_example.txt", true);
+  xhttp.send();
+}
