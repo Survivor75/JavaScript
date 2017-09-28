d30659a1357efd27787a964b551a8cc46f1cae5f
Survivor75 <ashutoshnarang80@gmail.com>
2017-09-29 02:38:37 +0530

    Bindings

--- AngularJS/ninjaBelts/src/app/home/home.component.ts
+++ AngularJS/ninjaBelts/src/app/home/home.component.ts
@@ -7,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   homeTitle = "Welcome To Ninja Belts"
-  myString = "I love chicken"``
-  alertMe(val){
-    alert(val)
-  }
+  ninja = {
+  name: "Yoshi",
+  belt: "Black"
+  };
+
   constructor() { }
 
   ngOnInit() {
