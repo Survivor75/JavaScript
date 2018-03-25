015ca4ef92bf6a082d009e90a8ebabffa14712cb
Survivor75 <ashutoshnarang80@gmail.com>
2018-03-25 12:25:30 +0530

    Seeing Sharp

--- C#/myApp/Program.cs
+++ C#/myApp/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using Acme.Collections;
 namespace myApp
 {
     class Program
@@ -7,6 +7,17 @@ namespace myApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Stack s = new Stack();
+            s.Push(1);
+            s.Push(10);
+            s.Push(100);
+            Console.WriteLine(s.Pop());
+            Console.WriteLine(s.Pop());
+            Console.WriteLine(s.Pop());
+
+            int i = 123;
+            object o = i;
+            int j = (int)o;        
         }
     }
 }
