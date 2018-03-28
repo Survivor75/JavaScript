8e6e318f76b0e2dfad7798763f8923e771817e57
Survivor75 <ashutoshnarang80@gmail.com>
2018-03-28 16:54:21 +0530

    Tour End

--- C#/myApp/geometry.cs
+++ C#/myApp/geometry.cs
@@ -244,3 +244,90 @@ class OverloadingExample
         F(1, 1);        // Invokes F(double, double)
     }
 }
+
+public class List<T>
+{
+    // Constant
+    const int defaultCapacity = 4;
+
+    // Fields
+    T[] items;
+    int count;
+
+    // Constructor
+    public List(int capacity = defaultCapacity) 
+    {
+        items = new T[capacity];
+    }
+
+    // Properties
+    public int Count => count; 
+
+    public int Capacity 
+    {
+        get { return items.Length; }
+        set 
+        {
+            if (value < count) value = count;
+            if (value != items.Length) 
+            {
+                T[] newItems = new T[value];
+                Array.Copy(items, 0, newItems, 0, count);
+                items = newItems;
+            }
+        }
+    }
+
+    // Indexer
+    public T this[int index] 
+    {
+        get 
+        {
+            return items[index];
+        }
+        set 
+        {
+            items[index] = value;
+            OnChanged();
+        }
+    }
+    
+    // Methods
+    public void Add(T item) 
+    {
+        if (count == Capacity) Capacity = count * 2;
+        items[count] = item;
+        count++;
+        OnChanged();
+    }
+    protected virtual void OnChanged() =>
+        Changed?.Invoke(this, EventArgs.Empty);
+
+    public override bool Equals(object other) =>
+        Equals(this, other as List<T>);
+
+    static bool Equals(List<T> a, List<T> b) 
+    {
+        if (Object.ReferenceEquals(a, null)) return Object.ReferenceEquals(b, null);
+        if (Object.ReferenceEquals(b, null) || a.count != b.count)
+            return false;
+        for (int i = 0; i < a.count; i++) 
+        {
+            if (!object.Equals(a.items[i], b.items[i])) 
+            {
+                return false;
+            }
+        }
+    return true;
+    }
+
+    // Event
+    public event EventHandler Changed;
+
+    // Operators
+    public static bool operator ==(List<T> a, List<T> b) => 
+        Equals(a, b);
+
+    public static bool operator !=(List<T> a, List<T> b) => 
+        !Equals(a, b);
+}
